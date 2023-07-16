b222d592ace44ddf59d3d2e37c597a45ea75c2a8
Arina Solovjova <arina@eosfintek.com>
2023-07-16 23:09:45 +0300

    style: configure font usage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{astro,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+			fontFamily: {
+				"gilmer-light": ['gilmer-light', 'gilmer-light'],
+				"gilmer-regular": ['gilmer-regular', 'gilmer-regular'],
+				"gilmer-heavy": ['gilmer-heavy', 'gilmer-heavy'],
+			},
+		},
   },
   plugins: [],
 };
