114735454f12d713ba8bac7203de2398a3cf0ce6
Arina Solovjova <arina@eosfintek.com>
2023-07-18 23:57:43 +0300

    refactor: format files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,12 @@ module.exports = {
   content: ['./src/**/*.{astro,ts,tsx}'],
   theme: {
     extend: {
-			fontFamily: {
-				"gilmer-light": ['gilmer-light', 'gilmer-light'],
-				"gilmer-regular": ['gilmer-regular', 'gilmer-regular'],
-				"gilmer-heavy": ['gilmer-heavy', 'gilmer-heavy'],
-			},
-		},
+      fontFamily: {
+        'gilmer-light': ['gilmer-light', 'gilmer-light'],
+        'gilmer-regular': ['gilmer-regular', 'gilmer-regular'],
+        'gilmer-heavy': ['gilmer-heavy', 'gilmer-heavy'],
+      },
+    },
   },
   plugins: [],
 };
