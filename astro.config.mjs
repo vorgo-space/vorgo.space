113a830966e9e17b41ec89f0c95cdeeff0727b04
poteresting <poteresting@gmail.com>
2023-07-23 23:02:45 +0300

    build: add react dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   experimental: {
-    assets: true,
+    assets: true
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()]
 });
