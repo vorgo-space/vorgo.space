5e4c6f26c08f5b9cf825cc52988554927c8aa685
Arina Solovjova <ari.solovjova@gmail.com>
2023-09-16 23:51:10 +0300

    refactor: remove comment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,10 +9,6 @@ module.exports = {
         'gilmer-heavy': ['gilmer-heavy', 'gilmer-heavy'],
         'gilmer-medium': ['gilmer-medium', 'gilmer-medium'],
       },
-      // backgroundImage: {
-      //   'dust-particles': "url('/src/assets/images/dust-particles.svg')",
-        // 'grid': "url('/src/assets/images/grid.svg')",
-      // },
     },
   },
   plugins: [],
